42853defcf4f97cc6a8269e77c4215a2d7a55b64
gmt9120 <gmt9120@stern.nyu.edu>
2022-07-11 17:35:02 -0400

    Validating User Inputs

--- game.py
+++ game.py
@@ -1,5 +1,33 @@
 # this is the "game.py" file...
 
+from multiprocessing import RLock
+from secrets import choice
+
+
 print("Rock, Paper, Scissors, Shoot!")
-x = input("Please choose rock, paper, or scissors and input it here:")
-print(x)
+user_input = input("Please choose rock, paper, or scissors and input it here:")
+print(user_input)
+def user_choice():
+    "Rock"
+    "Paper"
+    "Scissors"
+if user_input == "Rock":
+    user_choice == "Rock"
+elif user_input == "rock":
+    user_choice == "Rock"
+elif user_input == "ROCK":
+    user_choice == "Rock"
+elif user_input == "Paper":
+    user_choice == "Paper"
+elif user_input == "paper":
+    user_choice == "Paper"
+elif user_input == "PAPER":
+    user_choice == "Paper"
+elif user_input == "Scissors":
+    user_choice == "Scissors"
+elif user_input == "scissors":
+    user_choice == "Scissors"
+elif user_input == "SCISSORS":
+    user_choice == "Scissors"
+else:
+    quit()
