7ae67c2f57ee576cba9cf49b83880850eee15b97
gmt9120 <gmt9120@stern.nyu.edu>
2022-07-11 18:13:32 -0400

    Validating User Inputs_v3
    Switched to .lower() to capture all entry capitalizations. Added "not in" function to get errors

--- game.py
+++ game.py
@@ -3,32 +3,12 @@
 from multiprocessing import RLock
 from secrets import choice
 
-
 print("Rock, Paper, Scissors, Shoot!")
-user_input = input("Please choose rock, paper, or scissors and input it here:")
-print(user_input)
-def user_choice():
-    "Rock"
-    "Paper"
-    "Scissors"
-if user_input == "Rock":
-    user_choice == "Rock"
-elif user_input == "rock":
-    user_choice == "Rock"
-elif user_input == "ROCK":
-    user_choice == "Rock"
-elif user_input == "Paper":
-    user_choice == "Paper"
-elif user_input == "paper":
-    user_choice == "Paper"
-elif user_input == "PAPER":
-    user_choice == "Paper"
-elif user_input == "Scissors":
-    user_choice == "Scissors"
-elif user_input == "scissors":
-    user_choice == "Scissors"
-elif user_input == "SCISSORS":
-    user_choice == "Scissors"
-else:
-    quit()
-print(user_choice)
+
+### PROCESSING AND VALIDATING USER INPUTS ###
+
+user_choice = input("Please choose rock, paper, or scissors and input it here:").lower()
+print("You chose", user_choice, "!")
+if user_choice not in ["rock", "paper", "scissors"]:
+    print("Error. Please enter rock, paper, or scissors")
+    exit()
