f78eb51003bfd0b7a83fa41c0b20b9bfe5a61498
gmt9120 <gmt9120@stern.nyu.edu>
2022-07-11 18:30:38 -0400

    Determining the Winner
    Outlined logic of r/p/s results

--- game.py
+++ game.py
@@ -17,3 +17,19 @@ if user_choice not in ["rock", "paper", "scissors"]:
 
 computer_choice = choice(["rock", "paper", "scissors"])
 print("The Computer chose", computer_choice, "!")
+
+### DETERMINING THE WINNER ###
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("Rock beats scissors. You win!")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("Paper beats rock. You lose :(")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("Scissors beats paper. You lose :(")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("Paper beats rock. You win!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("Scissors beats paper. You win!")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("Rock beats scissors. You lose :(")
